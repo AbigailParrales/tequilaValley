85e73776f4a559399da85fb69830a43bf67b9156
AbigailParrales <danielaparralesd.m@gmail.com>
2020-11-07 15:24:06 -0600

    Adding Log In and Log Out and a rudimentary Profile + Resticting Profile feature to logged users

--- tequilaValley/users/views.py
+++ tequilaValley/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm    # Default Django form
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 from .forms import UserRegisterForm
 
 # Create your views here.
@@ -11,9 +13,15 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Yout account has been created, you are now able to log in!')
+            # Redirect user to home page
+            # return redirect('blog-home')
+            return redirect('login')
     else:
         # form = UserCreationForm() # Using Django default form
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
