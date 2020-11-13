93330bb817f2d68af06458792c0ed8e62212b1bf
AbigailParrales <danielaparralesd.m@gmail.com>
2020-11-12 22:27:24 -0600

    Detailing Posts: Udate, Delete and Detailed views

--- tequilaValley/blog/views.py
+++ tequilaValley/blog/views.py
@@ -1,8 +1,16 @@
 #from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
 from .models import Post
+
 
 """ posts = [
     {
@@ -29,6 +37,48 @@ def home(request):
     #return HttpResponse('<h1> Blog Home</h1>')
     #return render(request, 'blog/home.html')
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] # Order from latest to oldest
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):   # a form for creating a new post
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form): #Overwrite the user in the form
+        form.instance.author = self.request.user
+        return super().form_valid(form) # Verify the form
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    #Only logged users can update a post
+    def form_valid(self, form): #Overwrite the user in the form
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):    #Validate that only author can modify his post
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/blog/'
+
+    def test_func(self):    #Validate that only author can modify his post
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     #return HttpResponse('<h1> This is about page </h1>')
